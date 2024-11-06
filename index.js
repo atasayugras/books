cbf2c9092f46366f56a648eb20f473b47ef53e1f
Atasay Ugras <atasayugras@gmail.com>
2024-11-07 02:22:18 +0300

    lowercase p for public... inside index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 // General error handling middleware
